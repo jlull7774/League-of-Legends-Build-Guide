9161683c4f1b31f4140df1cb9f5d07145c256b01
Jason Lull <jlull7774@gmail.com>
2022-06-14 10:32:01 -0400

    added build controller

--- controllers/build.js
+++ controllers/build.js
@@ -29,7 +29,7 @@ router.use((req, res, next) => {
 
 router.get("/", (req, res) => {
   // find all the builds
-  Fruit.find({ username: req.session.username })
+  Build.find({ username: req.session.username })
     // render the template after they are found
     .then((builds) => {
       console.log(builds)
@@ -53,7 +53,7 @@ router.delete("/:id", (req, res) => {
   // get the id from params
   const id = req.params.id.toString().trim()
   // delete the build
-  Fruit.findByIdAndRemove(id)
+  Build.findByIdAndRemove(id)
     .then((build) => {
       // redirect to main page after deleting
       res.redirect("/builds")
@@ -73,7 +73,7 @@ router.put("/:id", (req, res) => {
   // check if the readyToEat property should be true or false
   req.body.readyToEat = req.body.readyToEat === "on" ? true : false
   // update the build
-  Fruit.findByIdAndUpdate(id, req.body, { new: true })
+  Build.findByIdAndUpdate(id, req.body, { new: true })
     .then((build) => {
       // redirect to main page after updating
       res.redirect("/builds")
@@ -93,7 +93,7 @@ router.post("/", (req, res) => {
   // add user to req.body to track related user
   req.body.username = req.session.username
   // create the new build
-  Fruit.create(req.body)
+  Build.create(req.body)
     .then((builds) => {
       // redirect user to index page if successfully created item
       res.redirect("/builds")
@@ -111,7 +111,7 @@ router.get("/:id/edit", (req, res) => {
   // get the id from params
   const id = req.params.id
   // get the build from the database
-  Fruit.findById(id)
+  Build.findById(id)
     .then((build) => {
       // render edit page and send build data
       res.render("builds/edit.liquid", { build })
@@ -130,7 +130,7 @@ router.get("/:id", (req, res) => {
   const id = req.params.id
 
   // find the particular build from the database
-  Fruit.findById(id)
+  Build.findById(id)
     .then((build) => {
       // render the template with the data from the database
       res.render("builds/show.liquid", { build })
