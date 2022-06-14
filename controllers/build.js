a8f53501c66751f50298a74c09bb3315e64f7574
Jason Lull <jlull7774@gmail.com>
2022-06-14 10:27:20 -0400

    added user schema

--- controllers/build.js
+++ controllers/build.js
@@ -52,9 +52,9 @@ router.get("/new", (req, res) => {
 router.delete("/:id", (req, res) => {
   // get the id from params
   const id = req.params.id.toString().trim()
-  // delete the fruit
+  // delete the build
   Fruit.findByIdAndRemove(id)
-    .then((fruit) => {
+    .then((build) => {
       // redirect to main page after deleting
       res.redirect("/builds")
     })
@@ -72,9 +72,9 @@ router.put("/:id", (req, res) => {
   const id = req.params.id.toString().trim()
   // check if the readyToEat property should be true or false
   req.body.readyToEat = req.body.readyToEat === "on" ? true : false
-  // update the fruit
+  // update the build
   Fruit.findByIdAndUpdate(id, req.body, { new: true })
-    .then((fruit) => {
+    .then((build) => {
       // redirect to main page after updating
       res.redirect("/builds")
     })
@@ -92,7 +92,7 @@ router.post("/", (req, res) => {
   req.body.readyToEat = req.body.readyToEat === "on" ? true : false
   // add user to req.body to track related user
   req.body.username = req.session.username
-  // create the new fruit
+  // create the new build
   Fruit.create(req.body)
     .then((builds) => {
       // redirect user to index page if successfully created item
@@ -110,11 +110,11 @@ router.post("/", (req, res) => {
 router.get("/:id/edit", (req, res) => {
   // get the id from params
   const id = req.params.id
-  // get the fruit from the database
+  // get the build from the database
   Fruit.findById(id)
-    .then((fruit) => {
-      // render edit page and send fruit data
-      res.render("builds/edit.liquid", { fruit })
+    .then((build) => {
+      // render edit page and send build data
+      res.render("builds/edit.liquid", { build })
     })
     // send error as json
     .catch((error) => {
@@ -129,11 +129,11 @@ router.get("/:id", (req, res) => {
   // get the id from params
   const id = req.params.id
 
-  // find the particular fruit from the database
+  // find the particular build from the database
   Fruit.findById(id)
-    .then((fruit) => {
+    .then((build) => {
       // render the template with the data from the database
-      res.render("builds/show.liquid", { fruit })
+      res.render("builds/show.liquid", { build })
     })
     .catch((error) => {
       console.log(error)
